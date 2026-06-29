7f32a70fb1c26db69c4b0faa1f6cb878e2962c46
naderuser <123259677+naderuser@users.noreply.github.com>
2026-06-29 15:03:24 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.activity:activity-compose:1.9.3")
-
+    implementation("androidx.appcompat:appcompat:1.7.0")
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.8.3")
 
